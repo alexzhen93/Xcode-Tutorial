9e38be72e5b3ec1b8e01cff093d84f67f7c4678e
Alex <alex_15_zhen@hotmail.com>
2023-02-06 16:29:39 -0500

    Initial Commit

---
+++ Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Alex Zhen on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
